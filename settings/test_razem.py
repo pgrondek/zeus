c2c7e68ce949d9596bc09aceea001a008db3a7e8
Grzesiek Kawka-Osik <mivalsten@gmail.com>
2023-04-05 21:52:16 +0000

    wtf

--- settings/test_razem.py
+++ settings/test_razem.py
@@ -1,16 +1,16 @@
 from .prod import *  # noqa
 
-ADMIN_NAME = 'KKW Razem'
-ADMIN_EMAIL = 'kkw@partiarazem.pl'
+ADMIN_NAME = 'TEST PKW Razem'
+ADMIN_EMAIL = 'razem.pkw@gmail.com'
 
 ADMINS = [
-    ('KKW Gmail', 'razem.pkw@gmail.com'),
+    ('PKW Gmail', 'razem.pkw@gmail.com'),
     ('Grzegorz Kawka-Osik', 'mivalsten@gmail.com'),
 ]
 ELECTION_ADMINS = ADMINS
 MANAGERS = ADMINS
 
-DEFAULT_FROM_EMAIL = 'kkw@partiarazem.pl'
+DEFAULT_FROM_EMAIL = 'kkw-test@partiarazem.pl'
 SERVER_EMAIL = '%s <%s>' % (DEFAULT_FROM_NAME, DEFAULT_FROM_EMAIL)
 
 HELP_EMAIL_ADDRESS = 'kkw@partiarazem.pl'
